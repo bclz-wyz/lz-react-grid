f9194cd31af7e9ba784c334ee92085eb59d6d259
weixiaozhu <442356120@qq.com>
2023-04-03 08:37:26 +0800

    feat:新增控制表格

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export { MsgDev } from './LJUtils';
 export { default as LJMoveable} from './LJMoveable'
 export { default as LJMoveableSelecto} from './LJMoveableSelecto'
 export { default as LJMoveableSelectoTest,LjMoveableChild,DemoLJMoveableSelectoTest} from './LJMoveableSelectoTest'
-
+export { default as LJTable} from './LJTable'
+export { default as LJTableSelect} from './LJTableSelect'
 
